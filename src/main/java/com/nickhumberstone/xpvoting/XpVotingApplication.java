a79c079a55f922d5f999d82a51d2ee37a023e2e2
nickhumberstone <nickhumberstone@gmail.com>
2024-08-13 18:26:16 +0100

    initial commmit - yay

---
+++ src/main/java/com/nickhumberstone/xpvoting/XpVotingApplication.java
@@ -0,0 +1,13 @@
+package com.nickhumberstone.xpvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpVotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickhumberstone/xpvoting/XpVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickhumberstone.xpvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
