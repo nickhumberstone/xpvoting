c2ade931a53a74ba89ffb7b438ec65b161c44d6a
Andrzej Prochyra <andrzej@prochyra.io>
2024-09-16 19:35:52 +0100

    fix: show "No topics yet" if there are no topics
    Closes #4

--- src/test/java/com/nickhumberstone/xpvoting/ProposalControllerIT.java
+++ src/test/java/com/nickhumberstone/xpvoting/ProposalControllerIT.java
@@ -6,7 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static java.util.Collections.emptyList;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProposalController.class)
@@ -22,5 +26,15 @@ class ProposalControllerIT {
     void should_return_200_for_home_page() throws Exception {
         mvc.perform(get("/"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void should_show_message_when_there_are_no_topics() throws Exception {
+        given(service.proposals())
+                .willReturn(emptyList());
+
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("No topics yet")));
     }
 }
