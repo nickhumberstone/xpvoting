9f7d3a475f67b0259635bb2afc8f8f95ba033af5
Andrzej Prochyra <andrzej@prochyra.io>
2024-11-27 12:24:15 +0000

    refactor: migrate to new @MockitoBean annotation since @MockBean is deprecated
    See https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.4-Release-Notes#deprecation-of-mockbean-and-spybean

--- src/test/java/com/nickhumberstone/xpvoting/ProposalControllerIT.java
+++ src/test/java/com/nickhumberstone/xpvoting/ProposalControllerIT.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static java.util.Collections.emptyList;
@@ -22,7 +22,7 @@ class ProposalControllerIT {
     @Autowired
     MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     ProposalService service;
 
     @Test
